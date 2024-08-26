d08637d05c9bce683ac3612ee58c825969f3704d
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-08-25 22:30:40 -0400

    feat: read more

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
+"use client";
+
 import Link from "./components/Link";
 import NextLink from "next/link";
 import Headshot from "@/app/assets/headshot.png";
 import Signature from "@/app/assets/signature.png";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function About() {
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
     <div className="flex flex-col gap-4">
       <p>CS @ University of Waterloo.</p>
@@ -25,22 +29,56 @@ export default function About() {
         , building systems to advance censorship-resistant Internet
         communications.
       </p>
-      <p>
-        In high school, I led the{" "}
-        <Link href="https://app.staugustinechs.ca">App Development Team</Link>{" "}
-        and grew our mobile app to hundreds of monthly active users. I developed
-        our high school&apos;s{" "}
-        <Link href="https://staugustinechs.ca">website</Link> (and{" "}
-        <Link href="https://app.staugustinechs.ca">
-          mobile app&apos;s website
-        </Link>{" "}
-        too).
-      </p>
+      {!isExpanded ? (
+        <button
+          className="text-neutral-700 font-medium text-left"
+          onClick={() => setIsExpanded(true)}
+        >
+          continue reading →
+        </button>
+      ) : (
+        <>
+          <p>
+            In high school, I led the{" "}
+            <Link href="https://app.staugustinechs.ca">
+              App Development Team
+            </Link>{" "}
+            and grew our mobile app to hundreds of monthly active users. I
+            developed our high school&apos;s{" "}
+            <Link href="https://staugustinechs.ca">website</Link> (and{" "}
+            <Link href="https://app.staugustinechs.ca">
+              mobile app&apos;s website
+            </Link>{" "}
+            too).
+          </p>
+          <p>
+            I was also an organizer of{" "}
+            <Link href="https://www.jamhacks.ca">JAMHacks</Link>, Canada&apos;s
+            largest high school hackathon. I was the head of the Web/Tech team,
+            where among other things, I spearheaded the development of the event
+            website and dashboard used by hundreds of participants.
+          </p>
+        </>
+      )}
       <NextLink
         href="/projects"
         className="text-center py-4 rounded-md border border-stone-700 hover:bg-stone-700 hover:text-neutral-200 bg-stone-100 text-neutral-500 transform transition-all duration-300"
       >
-        see what i&apos;ve built →
+        see what i&apos;ve built{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6 inline align-top"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+          />
+        </svg>
       </NextLink>
       <div className="flex justify-between">
         <div className="relative flex-1">
