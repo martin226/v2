2a0def91645de7a68b97a56b0071f9e4cb9feb57
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-08-26 12:49:45 -0400

    fix: responsive spacing

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex justify-center bg-stone-100 min-h-screen selection:bg-yellow-200 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-          <div className="flex flex-col gap-4 md:max-w-[500px] m-6 md:m-20 text-neutral-500 font-[380] md:mt-[100px] lg:mt-[180px]">
+        <main className="flex md:items-center justify-center bg-stone-100 min-h-screen selection:bg-yellow-200 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+          <div className="flex flex-col gap-4 md:max-w-[500px] m-6 md:m-20 text-neutral-500 font-[380]">
             <Header />
             {children}
             <Footer />
