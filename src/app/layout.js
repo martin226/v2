b46fba4a291f9774c544fb3ecef49569ecaae416
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-08-25 21:27:26 -0400

    feat: background dotted grid

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex justify-center bg-stone-100 min-h-screen selection:bg-yellow-200">
+        <main className="flex justify-center bg-stone-100 min-h-screen selection:bg-yellow-200 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
           <div className="flex flex-col gap-4 md:max-w-[500px] m-6 md:m-20 text-neutral-500 font-[380] md:mt-[100px] lg:mt-[180px]">
             <Header />
             {children}
