29cf424b0212cf09b9c4f8c647ca780e6434ecff
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-12-27 00:19:30 -0500

    feat: cs webring

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -11,7 +11,27 @@ export default function Footer({ className }) {
   return (
     <footer className={`flex flex-col gap-4 ${className}`}>
       <hr className="border-b border-neutral-200" />
-      <HorizontalNav links={links} />
+      <div className="flex-col sm:flex-row flex justify-between gap-4">
+        <HorizontalNav links={links} />
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <a href="https://cs.uwatering.com/#https://martinsit.ca/?nav=prev">
+            ←
+          </a>
+          <a
+            href="https://cs.uwatering.com/#https://martinsit.ca/"
+            target="_blank"
+          >
+            <img
+              src="https://cs.uwatering.com/icon.black.svg"
+              alt="CS Webring"
+              style={{ width: "24px", height: "auto", opacity: 0.8 }}
+            />
+          </a>
+          <a href="https://cs.uwatering.com/#https://martinsit.ca/?nav=next">
+            →
+          </a>
+        </div>
+      </div>
       <p>{new Date().getFullYear()} &copy; Martin Sit</p>
     </footer>
   );
