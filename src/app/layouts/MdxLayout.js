72d1ed1fd48fcd02a3947b00c4b85761b9b860ed
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-12-29 21:25:15 -0500

    feat: change blog heading font size

--- src/app/layouts/MdxLayout.js
+++ src/app/layouts/MdxLayout.js
@@ -1,6 +1,6 @@
 export default function MdxLayout({ children }) {
   return (
-    <div className="prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl">
+    <div className="prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-3xl">
       {children}
     </div>
   );
